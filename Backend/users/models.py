a32e7f86d862235c98fa3af895e4ff8d32dd8fd9
Juan Novas <juanignacionovas@gmail.com>
2024-10-04 19:29:35 -0300

    build(models): models files added

--- Backend/users/models.py
+++ Backend/users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from workgroups.models import Role, Department, Team
 
 # Create your models here.
+class Employee(models.Model):
+    person = models.ForeignKey(AbstractUser, on_delete=models.CASCADE)
+    start_date = models.DateField(null=True)
+    department = models.ForeignKey(Department, on_delete=models.SET_NULL)
+    team = models.ManyToManyField(Team)
+    role = models.ManyToManyField(Role)
--- Backend/vacations/models.py
+++ Backend/vacations/models.py
@@ -1,3 +1,22 @@
+from datetime import date
 from django.db import models
+from users.models import Employee
+from django.core.exceptions import ValidationError
 
 # Create your models here.
+class Vacation(models.Model):
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    start = models.DateField()
+    end = models.DateField()
+    
+    def clean(self):
+        super().clean()
+        if self.start > self.end:
+            raise ValidationError({
+                'end': "End date must be after start date."
+            })
+    
+    
+class VacationRequest(Vacation):
+    status = models.CharField(choices=[("Accepted", "A"),("Denied", "D"),("Pending", "P")])
+    message = models.CharField(max_length=500, null=True)
