d89350be115e2acc4ebff6bd45bdd6053e56b509
Juan Novas <juanignacionovas@gmail.com>
2024-10-04 19:49:23 -0300

    feat(admin): admin user creation on service up

---
+++ Backend/users/management/commands/createadmin.py
@@ -0,0 +1,21 @@
+import os
+from django.core.management.base import BaseCommand
+from users.models import User
+
+class Command(BaseCommand):
+    help = 'Create an admin user'
+
+    def handle(self, *args, **kwargs):
+        if not User.objects.filter(username=os.environ.get('ADMIN_USER')).exists():
+            user = User.objects.create_user(
+                username=os.environ.get('ADMIN_USER'),
+                email=os.environ.get('ADMIN_EMAIL'),
+                password = os.environ.get('ADMIN_PASSWORD')
+            )
+            user.is_superuser = True
+            user.is_staff = True
+            user.save()
+            
+            self.stdout.write(self.style.SUCCESS('Successfully created admin user'))
+        else:
+            self.stdout.write(self.style.WARNING('Admin user already exists'))
