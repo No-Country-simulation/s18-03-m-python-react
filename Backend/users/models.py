03fc0e79806e7382eb8e38fa6d9459569885e259
Juan Novas <juanignacionovas@gmail.com>
2024-10-04 19:37:49 -0300

    fix(models): django user model used corrected

--- Backend/users/models.py
+++ Backend/users/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
 from workgroups.models import Role, Department, Team
 
 # Create your models here.
 class Employee(models.Model):
-    person = models.ForeignKey(AbstractUser, on_delete=models.CASCADE)
+    person = models.ForeignKey(User, on_delete=models.CASCADE)
     start_date = models.DateField(null=True)
-    department = models.ForeignKey(Department, on_delete=models.SET_NULL)
+    department = models.ForeignKey(Department, on_delete=models.SET_NULL, null=True, blank=True)
     team = models.ManyToManyField(Team)
     role = models.ManyToManyField(Role)
