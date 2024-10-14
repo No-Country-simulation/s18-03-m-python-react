a46322d4cd93415fcd424bf7943114efa67f8ac6
anthony-xubuntu <abanon@fi.uba.ar>
2024-10-13 21:03:25 -0300

    We change the 'status' variable to read-only, we delete the update method.

--- Backend/vacations/serializers.py
+++ Backend/vacations/serializers.py
@@ -3,19 +3,13 @@ from .models import VacationRequest
 
 class VacationRequestSerializer(serializers.ModelSerializer):
     message = serializers.CharField(read_only=True)
+    status = serializers.CharField(read_only=True)
 
     class Meta:
         model = VacationRequest
         fields = ['start', 'end', 'status', 'message'] 
 
     def create(self, validated_data):
-        validated_data['employee'] = None  # Cuando se decida implementar la gestión de usuarios y autenticación en el futuro.
+        validated_data['employee'] = None  # For when you decide to implement user management and authentication in the future.
         vacation_request = VacationRequest.objects.create(**validated_data)
         return vacation_request
-
-    def update(self, instance, validated_data):
-        instance.start = validated_data.get('start', instance.start)
-        instance.end = validated_data.get('end', instance.end)
-        instance.status = validated_data.get('status', instance.status)
-        instance.save()
-        return instance
