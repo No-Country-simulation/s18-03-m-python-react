0c7883b2e3500d9bf8c271a7bca581c3ce3e2aaf
Juan Novas <juanignacionovas@gmail.com>
2024-10-18 11:55:39 -0300

    build(serializers): added to vacation response

--- Backend/vacations/serializers.py
+++ Backend/vacations/serializers.py
@@ -7,9 +7,26 @@ class VacationRequestSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = VacationRequest
-        fields = ['start', 'end', 'status', 'message'] 
+        fields = ['start', 'end', 'status', 'message']
 
     def create(self, validated_data):
         validated_data['employee'] = None  # For when you decide to implement user management and authentication in the future.
         vacation_request = VacationRequest.objects.create(**validated_data)
         return vacation_request
+    
+    
+class VacationResponseSerializer(serializers.Serializer):
+    vacation = serializers.IntegerField()
+    status = serializers.BooleanField()
+    note = serializers.CharField(max_length=500, required=False, allow_null=True)
+    
+    def validate_vacation(self, value):
+        try:
+            vacation_request = VacationRequest.objects.get(id=value)
+        except VacationRequest.DoesNotExist:
+            raise serializers.ValidationError("Vacation not found.")
+        
+        if vacation_request.status != "P":
+            raise serializers.ValidationError(f"Vacation alredy {vacation_request.get_status_display()}")
+        
+        return value
