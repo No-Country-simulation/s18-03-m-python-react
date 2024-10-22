8b18681bff59d98f43e74e4c1a7c50e9fba5dad5
anthony-xubuntu <abanon@fi.uba.ar>
2024-10-22 12:54:31 -0300

    Add vacation request endpoint

--- Backend/vacations/models.py
+++ Backend/vacations/models.py
@@ -5,7 +5,7 @@ from django.core.exceptions import ValidationError
 
 # Create your models here.
 class Vacation(models.Model):
-    employee = models.ForeignKey(Employee, on_delete=models.CASCADE, null=True)
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
     start = models.DateField()
     end = models.DateField()
     
