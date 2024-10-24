d9921eb46d4320b9eac6145003cccc64aecbd411
conrado <carlos_ag20@hotmail.com>
2024-10-23 23:57:49 -0300

    feat:vistas de vacaciones

--- frontend/app/(lng)/(routes)/vacation/page.tsx
+++ frontend/app/(lng)/(routes)/vacation/page.tsx
@@ -1,16 +1,11 @@
-import GeneralData from "@/components/molecules/GeneralData/GeneralData";
+
+import Vacation from "@/components/organisms/Vacation/Vacation";
 import React from "react";
 
 function PageVacation() {
   return (
-    <div id="section" className="flex flex-col w-1/3 gap-5 m-1 p-1">
-      <h1 className="text-3xl font-bold">datos del personal</h1>
-      <div className="flex flex-col gap-5 m-1 p-1 border-2 border-slate-400 ">
-      </div>
-      <h1 className="text-3xl font-bold">datos de localizacion</h1>
-      <div className="flex flex-col gap-5 m-1 p-1 border-2 border-slate-400">
-      </div>
-    </div>
+    <Vacation /> 
+   
   );
 }
 
