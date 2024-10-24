d1ff0a4449c4b0f5aa4c54a7569eb257900c7580
Juan Novas <juanignacionovas@gmail.com>
2024-10-24 18:31:42 -0300

    aa

--- Backend/assistances/models.py
+++ Backend/assistances/models.py
@@ -1,12 +1,14 @@
-from datetime import datetime
+import calendar
+from datetime import datetime, date
 from django.db import models
+from django.db.models.functions import TruncDate
 from django.core.exceptions import ValidationError
 from users.models import Employee
 
 # Create your models here.
 class Assistance(models.Model):
     employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
-    entry = models.DateTimeField(auto_now_add=True)
+    entry = models.DateTimeField()
     exit = models.DateTimeField(null=True, blank=True)
         
     def clean(self):
@@ -16,13 +18,46 @@ class Assistance(models.Model):
                 'exit': "Exit time must be after entry time."
             })
             
+
     @classmethod
-    def open_asistance(cls, employee_id):
-        today = datetime.today()
-        return cls.objects.filter(employee=employee_id, exit=None, entry__year=today.year, entry__month=today.month, entry__day=today.day).exists()
-    
-class AssistanceReport(models.Model):
-    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
-    note = models.CharField(max_length=500, null=True, blank=True)
-    is_justified = models.BooleanField()
-    date = models.DateField()
+    def get_report(cls, month: int=None, employee_id: int=None):
+        today = datetime.now()
+        if month is None:
+            month = today.month
+        if not 1 <= month <= 12:
+            raise ValueError
+        
+        cal = calendar.Calendar()
+        work_days = [
+            day[0] for day in cal.itermonthdays2(today.year, today.month)
+            if day[0] != 0 and day[1] < 5  and day[0] <= today.day
+        ]
+        
+        if employee_id is None:
+            employees_ids = Employee.objects.values_list("pk", flat=True)
+        else:
+            employees_ids = [employee_id]
+        
+        data = {}
+        
+        for id in employees_ids:
+            month_asistance = Assistance.objects.filter(
+                employee = id,
+                entry__year=today.year,
+                entry__month=today.month
+            ).annotate(date=TruncDate('entry')).values_list('date', flat=True)
+            
+            days_with_assistance = [assistance.day for assistance in month_asistance]
+            
+            days_without_assistance = [
+                day for day in work_days
+                if day not in days_with_assistance
+            ]
+            
+            dates_without_assistance = [
+                date(today.year, today.month, day) for day in days_without_assistance
+            ]
+            
+            data[id] =  dates_without_assistance
+        
+        return data
--- Backend/assistances/views.py
+++ Backend/assistances/views.py
@@ -2,12 +2,11 @@ from datetime import datetime
 from django.shortcuts import render
 from rest_framework import status
 from rest_framework.views import APIView
-from .serializer import AssistanceSerializer
+from rest_framework.response import Response
 from .models import Assistance
 
 # Create your views here.
-class AssistanceView(APIView):
-    def post(self, request, *args, **kwargs):
-        serializer = AssistanceSerializer(data=request.data)
-        
-        
+class AssistanceReportView(APIView):
+    def get(self, request, *args, **kwargs):
+        report = Assistance.get_report()
+        return Response({"message": report}, status=status.HTTP_200_OK)
